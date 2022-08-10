719e212c1f14dcd6e24342463d1ef27617b8cdb1
sercanfurunci <100974818+sercanfurunci@users.noreply.github.com>
2022-08-11 01:50:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor(Math.random()*6) + 1;
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var images1 = "images/dice"+ randomNumber1 +".png";
+var images2 = "images/dice"+ randomNumber2 +".png";
+
+document.querySelector(".dice .img1").setAttribute("src",images1);
+document.querySelector(".dice .img2").setAttribute("src",images2);
+
+
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "📢Player 1 wins!";
+}else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "Player 2 wins!📢";
+}else{
+    document.querySelector("h1").innerHTML = "Draw!";
+}
