6491b386cc9819c84fe9cbf54aae87f157d7406a
sercanfurunci <100974818+sercanfurunci@users.noreply.github.com>
2022-08-11 19:06:22 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-if (window.performance.navigation.type === 1) {
-   // if the page has been refreshed, call the rollDice() function.
-  rollDice();
-}
 
-function rollDice(){
+
+
 var randomNumber1 = Math.floor(Math.random()*6) + 1;
 var randomNumber2 = Math.floor(Math.random()*6) + 1;
 
@@ -13,9 +10,8 @@ var images2 = "dice"+ randomNumber2 +".png";
 document.querySelector(".dice .img1").setAttribute("src",images1);
 document.querySelector(".dice .img2").setAttribute("src",images2);
 changeTitle(randomNumber1, randomNumber2);
-}
 
-function changeTitle(randomNumber1, randomNumber2){
+
 if(randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML = "📢Player 1 wins!";
 }else if(randomNumber2 > randomNumber1){
@@ -23,4 +19,4 @@ if(randomNumber1 > randomNumber2){
 }else{
     document.querySelector("h1").innerHTML = "Draw!";
 }
-}
+
