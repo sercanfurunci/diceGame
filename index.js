8d0963f68203c12874b5cb159c007f0b304b6b62
sercanfurunci <100974818+sercanfurunci@users.noreply.github.com>
2022-08-11 19:02:01 +0300

    Update index.js

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ var randomNumber2 = Math.floor(Math.random()*6) + 1;
 var images1 = "dice"+ randomNumber1 +".png";
 var images2 = "dice"+ randomNumber2 +".png";
 
-document.querySelector("dice .img1").setAttribute("src",images1);
-document.querySelector("dice .img2").setAttribute("src",images2);
+document.querySelector(".dice .img1").setAttribute("src",images1);
+document.querySelector(".dice .img2").setAttribute("src",images2);
 changeTitle(randomNumber1, randomNumber2);
 }
 
